bfde665c82bcdae2f612b9481b0653bf00009296
Andrii Herasymenko <fylhsqvh@gmail.com>
2021-09-15 16:07:43 +0300

    Added comment to loadImage.hook

--- src/loadImage.hook.js
+++ src/loadImage.hook.js
@@ -1,12 +1,14 @@
-import { useState, useEffect } from 'react'
-import { loadImage } from './imagesLoaderSimulator'
+import { useState, useEffect } from "react";
+import { loadImage } from "./imagesLoaderSimulator";
+
+//Some comment
 
 export const useLoadImage = (id) => {
-    const [image, setImage] = useState(false)
+  const [image, setImage] = useState(false);
 
-    useEffect(() => {
-        loadImage(id).then(setImage)
-    }, [setImage, id])
+  useEffect(() => {
+    loadImage(id).then(setImage);
+  }, [setImage, id]);
 
-    return image
-}
+  return image;
+};
